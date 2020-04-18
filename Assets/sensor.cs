40d1bcf001d033c7010d9ae28301832d95b8f589
Javier Ramos <ram16230@uvg.edu.gt>
2020-04-18 10:59:44 -0600

    Get Rotation and Acceleration of device

---
+++ Assets/sensor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 GetAccelerometer()
+    {
+        Vector3 dir = Vector3.zero;
+        dir.y = -Input.acceleration.y;
+        dir.x = Input.acceleration.x;
+        return dir;
+    }
+
+    // Get Gyroscope
+    private Quaternion GetGyroscope()
+    {
+        Quaternion q = Input.gyro.attitude;
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
