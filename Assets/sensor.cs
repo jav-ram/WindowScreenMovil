fe8051d1951e095559c6f4ed6c0b5f1ac565ef29
Javier Ramos <ram16230@uvg.edu.gt>
2020-05-15 16:12:07 -0600

    Rotation recieved and reflect

--- Assets/sensor.cs
+++ Assets/sensor.cs
@@ -3,29 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class sensor : MonoBehaviour
+[System.Serializable]
+public class TCPmsg
+{
+    //public string tx;
+    //public string ty;
+    //public string tz;
+    
+    public string rx;
+    public string ry;
+    public string rz;
+    public string rw;
+}
+
+public class Sensor : MonoBehaviour
 {
     public Text Accelerometer_text;
     public Text Gyroscope_text;
+
+    private TCPmsg msg;
+    private TCP_Client client;
+    private Vector3 prevAcc;
     // Start is called before the first frame update
     void Start()
     {
+        msg = new TCPmsg();
         Input.gyro.enabled = true;
+        client = this.GetComponent<TCP_Client>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Accelerometer_text.text = GetAccelerometer().ToString();
-        Gyroscope_text.text = GetGyroscope().ToString();
-        Debug.Log(GetAccelerometer() + " - " + GetGyroscope());
+        Vector3 acc = GetAccelerometer();
+        Quaternion rot = GetGyroscope();
+
+        //Accelerometer_text.text = acc.ToString();
+        Gyroscope_text.text = rot.ToString();
+
+        // msg.tx = acc.x.ToString();
+        // msg.ty = acc.y.ToString();
+        // msg.tz = (-acc.z).ToString();
+
+        msg.rx = rot.x.ToString();
+        msg.ry = rot.y.ToString();
+        msg.rz = rot.z.ToString();
+        msg.rw = rot.w.ToString();
+
+        client.SendMessage(JsonUtility.ToJson(msg));
+
     }
 
     private Vector3 GetAccelerometer()
     {
-        Vector3 dir = Vector3.zero;
-        dir.y = -Input.acceleration.y;
-        dir.x = Input.acceleration.x;
+        Vector3 acc = Input.acceleration;
+        Vector3 dir = prevAcc - acc;
+        prevAcc = acc;
+        
+        Vector3 r = Vector3.Lerp(Vector3.zero, dir, dir.magnitude);
         return dir;
     }
 
